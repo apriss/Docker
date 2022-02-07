86ed9e86a067719258d8495d45ebbd98ec7b527e
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2022-02-07 14:29:02 +0700

    Create install-docker-focal.sh

---
+++ install-docker-focal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+myvariable=$USER
+sudo usermod -aG docker ${USER}
+
+sudo systemctl start docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo systemctl status docker
+docker-compose --version
