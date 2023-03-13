a732aa03d47086db7be0fd3f3312d31c0bd3b1c5
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2023-03-13 13:22:56 +0700

    Create docker-setup-jimmy.sh

---
+++ docker-setup-jimmy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+do apt remove docker docker-engine docker.io containerd runc
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker $USER
+newgrp docker
+
+curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
+
+chmod +x docker-compose-linux-x86_64
+
+sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+
+docker-compose version
